b10dead890e1d13e9731df4e16897cde574917f9
liyuan9889 <272030003@qq.com>
2021-03-01 15:57:31 +0800

    添加日志

--- src/main/java/com/tuhui/alarmpush/AlarmpushApplication.java
+++ src/main/java/com/tuhui/alarmpush/AlarmpushApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableScheduling
 @MapperScan("com.tuhui.alarmpush.mapper")
 public class AlarmpushApplication {
